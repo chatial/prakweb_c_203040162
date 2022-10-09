6f16ea21c9a94153e40c5fd28511cc45d7f8b5c8
chatial <chatialfc@gmail.com>
2022-10-09 23:19:07 +0700

    Ngoding #6

---
+++ oophp/overriding.php
@@ -0,0 +1,75 @@
+<?php 
+
+class Produk {
+    public $judul, 
+           $penulis,
+           $penerbit,
+           $harga;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk() {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+
+}
+
+
+class Komik extends Produk {
+    public $jmlHalaman;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0 ) {
+
+        parent::__construct( $judul, $penulis, $penerbit, $harga );
+
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfoProduk() {
+        $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+
+class asal extends Produk {
+    public $waktuMain;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jam = 0 ) {
+        parent::__construct( $judul, $penulis, $penerbit, $harga );
+
+        $this->jam = $jam;
+    }
+
+    public function getInfoProduk() {
+        $str = "jam : " . parent::getInfoProduk() . " ~ {$this->jam} jam.";
+        return $str;
+    }
+}
+
+
+class CetakInfoProduk {
+    public function cetak( Produk $produk ) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000,100,0 ,"Komik");
+$produk2 = new asal("Uncharted","Neil Druckmann","Sony Computer", 250000, 120, 50, "Game");
+
+echo $produk1->getInfoProduk();
+echo "<Hr>";
+echo $produk2->getInfoProduk();
